700012d49cbbcd901c2084af1eecdf2e6d6b5ddf
jjimho439 <fantitasingas@gmail.com>
2026-05-13 08:34:31 +0200

    Fix bugs

--- src/main/java/ies/alcores/daweb/model/Asignatura.java
+++ src/main/java/ies/alcores/daweb/model/Asignatura.java
@@ -2,6 +2,7 @@ package ies.alcores.daweb.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,6 +21,7 @@ public class Asignatura {
     private final int horas;
 
     @ManyToOne
+    @JoinColumn(name = "imparte")
     private final Profesor profesor;
 
 }
